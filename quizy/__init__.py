46eaa3262653d0703b9e635265fb1cd4700d7bd3
rustam <kerimov.rustam@live.ru>
2025-11-30 14:03:50 +0100

    Bump version to 0.1.4

--- quizy/__init__.py
+++ quizy/__init__.py
@@ -6,5 +6,5 @@ from .core import Quiz, Question
 from .quiz_102 import quiz_102
 from .quiz_101 import quiz_101
 
-__version__ = "0.1.0"
+__version__ = "0.1.4"
 __all__ = ["Quiz", "Question", "quiz_102", "quiz_101"]
