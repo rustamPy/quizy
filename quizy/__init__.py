f7ea21634d60e6ceaa809f3019f7f88f1898db5b
rustam <kerimov.rustam@live.ru>
2025-12-14 20:28:09 +0100

    Bump version to 0.4.5

--- quizy/__init__.py
+++ quizy/__init__.py
@@ -20,7 +20,7 @@ from .cli import QuizCLI, TimerDisplay
 from .quiz_101 import quiz_101
 from .quiz_102 import quiz_102
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 __all__ = [
     "Quiz",
     "Question",
